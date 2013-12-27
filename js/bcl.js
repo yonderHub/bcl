03a93364caab6d0059f0cd49c2128164eb873b16
Dave Michelson <daveism@gmail.com>
2013-12-27 13:06:58 -0500

    commit for loops
    loops api running on local machine.  need to move to server.

--- js/bcl.js
+++ js/bcl.js
@@ -1,19 +1,33 @@
 BCL = {};
 var BCL  = {
 	createloop:function(aNode,aDistance){
-		url = "http://192.168.1.103:3000/bcl/loop/"+aNode+"/"+aDistance;
-
+		urlStr = "http://192.168.1.103:3000/bcl/loop/"+aNode+"/"+aDistance;
+		$('#bLoop').html('Create Another Loop <span class="glyphicon glyphicon-exclamation-sign"></span>');
 	    $.ajax({
+	    	type : "GET",
 	        url: urlStr,
-	        dataType: "json",
-	        data: data,
-	         crossDomain: true,
-	         success:function(response){this.addtomap(response);},
-	         error:function(x,t,m){console.log('fail');}
+	        dataType: "jsonp",
+	        crossDomain: true,
+	        success:function(data){BCL.addtomap(data);},
+	        error:function(x,t,m){console.log('fail ' + x + '-' + t + '-' +  m);},
 	     });
 
 	},
 	addtomap:function(gjson){
+		BCL.clearMap()
 		geojsonLayer = L.geoJson(gjson).addTo(map);
-	}
+		map.fitBounds(geojsonLayer.getBounds());
+	},
+	clearMap:function() {
+	    for(i in map._layers) {
+	        if(map._layers[i]._path != undefined) {
+	            try {
+	                map.removeLayer(map._layers[i]);
+	            }
+	            catch(e) {
+	                //do nothing....
+	            }
+	        }
+	    }
+}
 }
